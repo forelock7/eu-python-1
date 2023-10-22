3028c2c7c120c9390f471424935e42497d9376f1
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-10-22 21:54:06 +0300

    Start Task 8

---
+++ task_8/shop.py
@@ -0,0 +1,52 @@
+class Shop():
+    def __init__(self, shop_name, store_type):
+        self.shop_name = shop_name
+        self.store_type = store_type
+        self.number_of_units = 0
+    def describe_shop(self):
+        print(f"'{self.shop_name}' ({self.store_type})")
+    def open_shop(self):
+        print(f"Online shop '{self.shop_name}' is open")
+    def set_number_of_units(self, number_of_units):
+        self.number_of_units = number_of_units
+    def increment_number_of_units(self, number):
+        self.number_of_units += number
+
+class Discount(Shop):
+    def __init__(self, shop_name, store_type, discount_products):
+        super().__init__(shop_name, store_type)
+        self.discount_products = discount_products
+    def get_discounts_ptoducts(self):
+        print(f"Products: {self.discount_products}")
+
+# # Task 8.1a
+# print("*** Task 8.1a ***")
+# store = Shop('Silpo', 'grocery store')
+# print(store.shop_name)
+# print(store.store_type)
+# store.open_shop()
+# store.describe_shop()
+# # Task 8.1b
+# print("*** Task 8.1b ***")
+# store2 = Shop('Fora', 'grocery store')
+# store3 = Shop('Foxtrot', 'electronics store')
+# store4 = Shop('OLX', 'marketplace')
+# store2.describe_shop()
+# store3.describe_shop()
+# store4.describe_shop()
+# # Task 8.1c
+# print("*** Task 8.1c ***")
+# store_c = Shop('Silpo', 'grocery store')
+# print("number_of_units =", store_c.number_of_units)
+# store_c.number_of_units = 5
+# print("number_of_units =", store_c.number_of_units)
+# # Task 8.1d
+# print("*** Task 8.1d ***")
+# store_c.set_number_of_units(77)
+# print("number_of_units =", store_c.number_of_units)
+# store_c.increment_number_of_units(11)
+# print("number_of_units =", store_c.number_of_units)
+# # Task 8.1e
+# print("*** Task 8.1e ***")
+# store_discount = Discount('Foxtrot', 'electronics store', ['Iphone', 'Apple Watch', 'Ipad'])
+# store_discount.get_discounts_ptoducts()
---
+++ task_8/task_8_1f.py
@@ -0,0 +1,6 @@
+from task_8.shop import Shop
+
+# Task 8.1f
+all_store = Shop('OLX', 'marketplace')
+print('*** Task 8.1f ***')
+all_store.describe_shop()
